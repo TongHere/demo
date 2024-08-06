9cc64c578169f7cae61c6639f3853a2968af3c74
Tong <95514305+TongHere@users.noreply.github.com>
2024-08-06 15:30:43 +0200

    modify prompt

--- app_singleshot.py
+++ app_singleshot.py
@@ -28,8 +28,7 @@ def main():
     load_dotenv()
     st.set_page_config(page_title="Add Context Component Prompt", page_icon="🤖",layout="wide")
     if 'prompt' not in st.session_state:
-        st.session_state.prompt = """As a marketing manager, briefly introducing our new software product and offering a special discount for early adopters to potential clients. Include a special discount offer for early adopters. Use bullet points like:. Include a special discount offer for early adopters. Use bullet points like:
-Achieved [Objective] by implementing [Action/Measure], resulting in [Outcome/Impact].
+        st.session_state.prompt = """As a marketing manager, briefly introducing our new software product and offering a special discount for early adopters to potential clients. Include a special discount offer for early adopters. Use bullet points like:. Include a special discount offer for early adopters. Use bullet points like: Achieved [Objective] by implementing [Action/Measure], resulting in [Outcome/Impact].
 """
 
     # Text area for the prompt with pre-filled text
